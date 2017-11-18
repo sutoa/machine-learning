4ac475a638b74776901df3a29b37d85cb0c3a920
tong su <tong_su@hotmail.com>
2017-11-18 18:22:16 -0500

    Regularized Logistic Regression Model - added regularized adjustment

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,8 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
+h = sigmoid(X * theta);
+J = (-y' * log(h) - (1 - y)' * log(1 - h)) / m + (theta' * theta) * lambda / (2 * m);
+grad = X' * (h - y); 
+grad_reg_adj = lambda * theta;
+grad = (grad + grad_reg_adj) / m;
 
 
 
